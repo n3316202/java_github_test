a1651346557497573f9e2f8eee69eae7087e09fc
tom <nqwrt@naver.ocm>
2023-03-09 12:09:35 +0900

    네번재 커밋

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,8 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		//System.out.println("Hello");
-		//System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 
 	}
 
